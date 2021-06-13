ba1bb8e1a5cf15e864035c6b371f3046f5a218ef
Jiwon Park <park@jiwon.me>
2021-06-13 20:56:03 +0900

    add ALLOWED_HOSTS *

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -27,12 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    '3.37.109.0',  # aws lightsail
-    'orcinus.jiwon.me',
-    'orcinusapi.web.app'
+    '*'
 ]
 
 
