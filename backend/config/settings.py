f717f8dfd645602c5eb713e71b0af3f0662daf60
Jiwon Park <park@jiwon.me>
2021-06-14 13:36:23 +0900

    static root setting

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -152,6 +152,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, '.static_root')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
